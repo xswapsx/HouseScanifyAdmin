37de598d26b46bbeccc4b38f4b8ad070a730c78b
swapnil lanjewar <xswapsx@users.noreply.github.com>
2022-05-28 18:46:33 +0530

    cleanups

--- app/src/main/java/com/appynitty/adminapp/activities/DashboardActivity.java
+++ app/src/main/java/com/appynitty/adminapp/activities/DashboardActivity.java
@@ -44,7 +44,6 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_dashboard1);
         init();
     }
 
